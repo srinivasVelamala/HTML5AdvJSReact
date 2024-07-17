d670c88ecb83367f49246e54c48be4d6c6557da2
srinivasv18 <srinivasv18@gmail.com>
2024-07-17 11:12:48 +0530

    adv js

--- AdvanceJSandTS/ArraySample.js
+++ AdvanceJSandTS/ArraySample.js
@@ -1,8 +1,12 @@
 let numbers=[1,2,1,2,3,2,3,1];
-if(numbers[0]==numbers[2])
+if(numbers[0]==numbers[1])
 {
 console.log("Correct!");
+document.getElementById("demo").innerHTML =
+    "Correct!";
 }
 else {
 console.log("Wrong, please try again");
+document.getElementById("demo").innerHTML =
+    "Wrong, please try again";
 }
